9d6890ceaa558fc128997ac1ca3e5de3273b1a39
YoanMen <yoan.menard.dev@gmail.com>
2024-08-30 08:58:48 +0000

    add about section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'about' => "Mon parcours personnel est étroitement lié à ma passion pour la programmation, une passion qui a commencé il y a plusieurs années comme un simple passe-temps. Initialement attiré par la création de jeux vidéo, j'ai découvert Flutter, ce qui m'a incité à apprendre à développer mes propres applications. Après plusieurs années d'apprentissage et de pratique, j'ai décidé de transformer cette passion en profession. C'est ainsi que j'ai entrepris une reconversion vers le métier de développeur Web et mobile. Actuellement, je suis en formation pour obtenir le titre de Développeur Web et Mobile (DWWM), ce qui m'a permis de découvrir de nouveaux horizons et d'acquérir des compétences solides.",
         ]);
     }
 }
