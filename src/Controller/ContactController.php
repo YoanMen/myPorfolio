966ebb65eb4ff931985879ce24dcac193a5b54dd
YoanMen <yoan.menard.dev@gmail.com>
2024-08-30 14:20:43 +0000

    feat: add back-end logic to send mail from contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\ContactDTO;
+use App\Service\MailerService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', methods: ['POST'], name: 'app_contact')]
+    public function index(Request $request, ValidatorInterface $validator, MailerService $mailerService): JsonResponse
+    {
+        $content = $request->getContent();
+        $data = json_decode($content, true);
+
+        $sender = new ContactDTO();
+        $sender->name = $data['name'];
+        $sender->email = $data['email'];
+        $sender->message = $data['message'];
+
+        $errors = $validator->validate($sender);
+        if (!count($errors) > 0) {
+            try {
+                $mailerService->sendContactEmail($sender);
+
+                return new JsonResponse(['success' => $mailerService], 200);
+            } catch (\Exception $e) {
+                return new JsonResponse(['success' => false, 'error' => 'Cant sent your mail internal error'], 500);
+            }
+        } else {
+            return new JsonResponse(['success' => false, 'error' => (string) $errors], 405);
+        }
+    }
+}
---
+++ src/Service/MailerService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\ContactDTO;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class MailerService
+{
+  public function __construct(private MailerInterface $mailer) {}
+
+  public function sendContactEmail(ContactDTO $sender): void
+  {
+    $mail = (new TemplatedEmail())
+      ->to($_ENV['CONTACT_MAIL'])
+      ->subject('Message du portfolio par ' . $sender->name)
+      ->from($_ENV['NOREPLY_MAIL'])
+      ->htmlTemplate('email/contact.html.twig')
+      ->context(['data' => $sender]);
+
+    $this->mailer->send($mail);
+  }
+}
