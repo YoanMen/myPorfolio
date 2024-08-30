2149b8705e7bc788511023de120c4b62e25d684e
YoanMen <yoan.menard.dev@gmail.com>
2024-08-30 08:19:59 +0000

    implement hero

--- assets/build/react/controllers/LinkItem.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function LinkItem() {
-  return /*#__PURE__*/React.createElement("li", null, "test");
-}
--- assets/react/controllers/LinkItem.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LinkItem() {
-  return <li>test</li>;
-}
